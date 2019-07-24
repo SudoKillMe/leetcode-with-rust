5d202dd032c26d0fb00bbd4a884f7d905ff5f890
TecherLang <348262038@qq.com>
2019-07-24 15:05:13 +0800

    modify

--- sqrtx/src/main.rs
+++ sqrtx/src/main.rs
@@ -1,39 +1,39 @@
+use std::{thread, time};
 
-fn my_sqrt (x: i32) -> i32 {
-
+fn my_sqrt(x: i32) -> i32 {
     let mut left = 0;
     let mut right = x;
 
     while left < right {
-        let middle = (left + right) >> 1;
+        let middle = (left + right + 1) >> 1;
+        let square = middle * middle;
 
-        if middle * middle == x {
+        if square == x {
             return middle as i32;
         }
 
-        if middle * middle < x {
+        if square < x {
             left = middle;
         } else {
             right = middle - 1;
-        }   
+        }
     }
 
     left as i32
-
 }
 
 #[test]
-fn test () {
+fn test() {
     let x = 4;
     assert_eq!(my_sqrt(x), 2);
 }
 
 #[test]
-fn test_one () {
+fn test_one() {
     let x = 8;
     assert_eq!(my_sqrt(x), 2);
 }
 
 fn main() {
-    println!("Hello, world!");
+    my_sqrt(8);
 }
